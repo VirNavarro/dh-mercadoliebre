5db84288297ac03520312eaffdf8d12b084f3593
VirNavarro <virginiajnavarro@gmail.com>
2022-04-03 01:46:26 -0300

    Subiendo mercado liebre

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log("Levantando servidor en el puerto: ", PORT);
+});
+
+app.get("/", (req, res) => {
+  let route = path.resolve(__dirname, "./views/home.html");
+  res.sendFile(route);
+});
+
+app.get("/register", (req, res) => {
+  let route = path.resolve(__dirname, "./views/registro.html");
+  res.sendFile(route);
+});
+
+app.get("/login", (req, res) => {
+  let route = path.resolve(__dirname, "./views/login.html");
+  res.sendFile(route);
+});
