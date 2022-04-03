65dae04e21cd9d847b2296c6cf6d0c7f479c6aef
VirNavarro <virginiajnavarro@gmail.com>
2022-04-03 02:19:16 -0300

    Add heroku config

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
 
 app.use(express.static("public"));
 
